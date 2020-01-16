956f0f00dae18ac69ee44a72f4f2cd2730441ea6
stepanenko <serhiy.stepanenko@aiesec.net>
2020-01-16 12:36:21 +0200

    done

--- test/appTest.js
+++ test/appTest.js
@@ -1,40 +1,44 @@
 
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
 const addNumbers = require('../app').addNumbers;
-// const app = require('../app');
+const app = require('../app');
+
+// Results:
+const sayHelloResult = app.sayHello();
 
 describe('App', function() {
-  it('sayHello sould return hello', function() {
-    let result = sayHello();
-    assert.equal(result, 'hello');
+  describe('sayHello()', function() {
+    it('sould return hello message', function() {
+      assert.equal(sayHelloResult, 'hello');
+    });
+  
+    it('should return type string', function() {
+      assert.typeOf(sayHelloResult, 'string');
+    });
   });
-
-  it('sayHello should return type string', function() {
-    let result = sayHello();
-    assert.typeOf(result, 'string');
-  });
-
-  it('addNumbers should return a number', function() {
-    let result = addNumbers(1, 3);
-    assert.isNumber(result);
-  });
-
-  it('addNumbers should add two numbers', function() {
-    const a = 3;
-    const b = -1;
-    const result = addNumbers(a, b);
-    assert.equal(result, 2);
-  });
-
-  it('addNumbers should be above 5', function() {
-    const result = addNumbers(3, 4);
-    assert.isAbove(result, 5);
-  });
-
-  it('addNumbers should return a type number', function() {
-    const result = addNumbers('5', 8);    // AssertionError: expected '58' to be a number
-    assert.typeOf(result, 'number');
+  
+  describe('addNumbers()', function() {
+    it('should return a number', function() {
+      let result = addNumbers(1, 3);
+      assert.isNumber(result);
+    });
+  
+    it('should add two numbers', function() {
+      const a = 3;
+      const b = -1;
+      const result = addNumbers(a, b);
+      assert.equal(result, 2);
+    });
+  
+    it('should be above 5', function() {
+      const result = addNumbers(3, 4);
+      assert.isAbove(result, 5);
+    });
+  
+    it('should return a type number', function() {
+      const result = addNumbers('5', 8);    // AssertionError: expected '58' to be a number
+      assert.typeOf(result, 'number');
+    });
   });
 
 });
